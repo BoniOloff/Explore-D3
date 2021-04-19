488aef2d21f5113564e9ffead180df687086eb2f
Boni Nugraha <bonioloff@gmail.com>
2021-04-19 17:30:45 -0500

    Use JSON as data source.

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
 const canvas = d3.select(".canva");
+canvas.attr("width", "600")
+    .attr("height", "600");
+
+// var dataArray = [4, 15, 34, 12];
+
+// var dataArray = [
+//     {width: 25, height: 4, fill: "pink"},
+//     {width: 25, height: 14, fill: "purple"},
+//     {width: 25, height: 44, fill: "orange"},
+//     {width: 25, height: 124, fill: "green"},
+//     {width: 25, height: 12, fill: "grey"},
+//     {width: 25, height: 34, fill: "red"}
+// ]
 
 const svg = canvas.append("svg")
-            .attr("width", "600")
-            .attr("height", "600");
+            .attr("width", "1000")
+            .attr("height", "1000");
 
-svg.append("circle")
-    .attr("cx", "100")
-    .attr("cy", "100")
-    .attr("r", "50")
-    .attr("fill", "blue");
+const rect = svg.selectAll("rect");
 
-svg.append("rect")
-    .attr("width", "100")
-    .attr("height", "100")
-    .attr("x", "150")
-    .attr("y", "50")
-    .attr("fill", "green")
-    .attr("rx", "15")
-    .attr("ry", "15");
 
-svg.append("line")
-    .attr("x1", "9")
-    .attr("x2", "45")
-    .attr("y1", "300")
-    .attr("y2", "46")
-    .attr("stroke", "grey")
-
-svg.append("text")
-    .text("Hello there")
-    .attr("x", "100")
-    .attr("y", "200")
-    .attr("fill", "grey")
-    .attr("font-size", "30")
+var dataArray = d3.json("data.json")
+                  .then(data => {
     
+    rect.data(data)
+        .enter().append("rect")
+        .attr("width", "49")
+        .attr("fill", d => d.fill)
+        .attr("height", function(d){
+            return d.height * 2;
+        })
+        .attr("x", (d, i) => i * 50)
+        .attr("y", (d, i) => {
+            return 700 - (d.height * 2);
+        });
+})
